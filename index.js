aa550e3317ee03d7d0a016966f1a7bd9346bc2aa
Ivan Krutov <vania-pooh@aerokube.com>
2021-07-31 10:26:08 +0300

    Using wss:// instead of ws://

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const username = 'test';
 const password = 'test-password';
 
 (async () => {
-    const browser = await firefox.connect({ timeout: 0, wsEndpoint: `ws://${username}:${password}@${host}/playwright/firefox/playwright-1.12.2` });
+    const browser = await firefox.connect({ timeout: 0, wsEndpoint: `wss://${username}:${password}@${host}/playwright/firefox/playwright-1.12.2` });
     const page = await browser.newPage();
     await page.goto('https://aerokube.com/moon/');
     await page.screenshot({ path: `screenshot.png` });
