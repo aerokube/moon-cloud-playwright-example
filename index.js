5ab2db56438f01cfac2ee9f72e69cfea7ef9991b
Ivan Krutov <vania-pooh@aerokube.com>
2021-09-24 17:39:03 +0300

    Bump Playwright to 1.15.0

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 
 const { firefox } = require('playwright');
 
-const host = 'my-cluster.cloud.aerokube.com';
-const username = 'test';
-const password = 'test-password';
+const host = 'automation-remarks.cloud.aerokube.com';
+const username = 'automation-remarks';
+const password = 'S7sOsXQfArTu3yGP9jaBvWz4';
 
 (async () => {
-    const browser = await firefox.connect({ timeout: 0, wsEndpoint: `wss://${username}:${password}@${host}/playwright/firefox/playwright-1.12.2` });
+    const browser = await firefox.connect({ timeout: 0, wsEndpoint: `wss://${username}:${password}@${host}/playwright/firefox/playwright-1.15.0` });
     const page = await browser.newPage();
     await page.goto('https://aerokube.com/moon/');
     await page.screenshot({ path: `screenshot.png` });
