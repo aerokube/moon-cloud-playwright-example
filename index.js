dfd6635e97a96b43b18c8b17a43ff4a3038bed2e
Ivan Krutov <vania-pooh@aerokube.com>
2022-02-22 11:59:20 +0300

    Fixed access URL too

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const username = 'my-company';
 const password = 'some-password';
 
 (async () => {
-    const browser = await firefox.connect({ timeout: 0, wsEndpoint: `wss://${username}:${password}@${host}/playwright/firefox/playwright-1.15.0` });
+    const browser = await firefox.connect({ timeout: 0, wsEndpoint: `wss://${username}:${password}@${host}/playwright/firefox/playwright-1.19.1` });
     const page = await browser.newPage();
     await page.goto('https://aerokube.com/moon/');
     await page.screenshot({ path: `screenshot.png` });
